5ef696a78a29770b6f1ba4e3ba4a1100f769295c
Jyotish kushwaha <jyotishkushwaha970@gmail.com>
2025-09-09 11:38:46 +0530

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -2,7 +2,7 @@ import nlpcloud
 
 class MyAPI:
     def sentiment_analysis(self, text):
-        client = nlpcloud.Client("finetuned-llama-3-70b", "b44e989ef9d1294f119aa616a239a323c75257be", gpu=True)
+        client = nlpcloud.Client("finetuned-llama-3-70b", "283dc9f5c36fe176aad3b90f8e649470b639fa43", gpu=True)
         d = client.sentiment(text,target='NLP Cloud')
         response=d['scored_labels'][1]['label']
         if response is None:
@@ -10,7 +10,7 @@ class MyAPI:
         return response
 
     def language_detection(self,text):
-        client = nlpcloud.Client("python-langdetect", "0292b9a164d1f648374ec2481a1c22bee43d5ad5", gpu=False)
+        client = nlpcloud.Client("python-langdetect", "283dc9f5c36fe176aad3b90f8e649470b639fa43", gpu=False)
         language=client.langdetection(text)
         response=language['languages'][0]
         print(response)
@@ -18,7 +18,7 @@ class MyAPI:
     
             
     def semantic_analysis(self, text1, text2):
-        client = nlpcloud.Client("paraphrase-multilingual-mpnet-base-v2", "0292b9a164d1f648374ec2481a1c22bee43d5ad5", gpu=False)
+        client = nlpcloud.Client("paraphrase-multilingual-mpnet-base-v2", "283dc9f5c36fe176aad3b90f8e649470b639fa43", gpu=False)
         response = client.semantic_similarity([text1, text2])
         if response is None:
             return "Error: No response from NLP Cloud"
